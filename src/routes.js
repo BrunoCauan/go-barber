c5d038fa220c62179d7b8efd4b5b0cc89105b158
BrunoCauan <brunocaua2000@gmail.com>
2019-09-23 10:02:08 -0300

    first commit'

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import Appointment from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationsController from './app/controllers/NotificationsController';
+import AvailableController from './app/controllers/AvailableController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', Appointment.index);
+routes.post('/appointments', Appointment.store);
+routes.delete('/appointments/:id', Appointment.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationsController.index);
+routes.put('/notifications/:id', NotificationsController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
